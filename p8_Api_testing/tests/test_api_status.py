e33da00224f88b93e7afaa91f335a0d26ba57cbd
PaulaIoanaOprea <shnnaya_91@yahoo.com>
2023-05-15 21:30:56 +0300

    Agaudare curs 15

---
+++ p8_Api_testing/tests/test_api_status.py
@@ -0,0 +1,20 @@
+import unittest
+
+from p8_Api_testing.requests_folder.simple_books_requests import SimpleBooksRequests
+
+
+class TestApiStatus(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.books_api = SimpleBooksRequests()
+
+    def test_api_status(self):
+        response = self.books_api.get_api_status()
+
+        expected_response_code = 200
+        actual_response_code = response.status_code
+
+        self.assertEqual(expected_response_code, actual_response_code, "Error, unexpected status code")
+
+        expected_response_message = "OK"
+        actual_response_message = response.json()["status"]
